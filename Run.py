cce557a1d8359ff99f8f2d8de3069d8a1b3a9be3
Eric Chen <ericchen@Eric-Chens-MacBook-Pro.local>
2016-10-01 21:33:35 -0700

    changes to run

--- Run.py
+++ Run.py
@@ -23,6 +23,12 @@ while True:
 			user = getUser(line)
 			message = getMessage(line)
 			words=message.split()
+			if "PING" in message:
+				PONG(s)
+				#s.send(line.replace("PING", "PONG"))
+			if "Kappa" in  message:
+				kappaCount+=1
+				#sendMessage(s,str(kappaCount)+ " kappas")
 			if("*" not in message or user !="z03hboot"):
 				fillQueue(message)
 				# addNew(message) #####
@@ -30,20 +36,7 @@ while True:
 				if(user=="zo3h"):
 					print("Redo")
 					sendMessage(s,"GivePLZ Restarting TakeNRG")
-					execv(sys.executable, ['python'] + sys.argv)	
-			if "Kappa" in  message:
-				kappaCount+=1
-				#sendMessage(s,str(kappaCount)+ " kappas")
-				break
-			if "*ping" in  message:
-				try:
-				    if (words[1] == "pong"):
-					sendMessage(s,"kappa")
-				except Exception, e:
-				    print("no param")
-
-				sendMessage(s,"PONG!")
-				break
+					execv(sys.executable, ['python'] + sys.argv)
 			if "*shutdown" in message:
 				user = getUser(line);
 				if(user == "zo3h"):
@@ -52,7 +45,17 @@ while True:
 					sys.exit();
 				else:
 					#sendMessage(s,"how about no")
-					break
+					break	
+			if "*ping" in  message:
+				try:
+				    if (words[1]):
+						sendMessage(s,"pong "+words[1])
+						break
+				except Exception, e:
+				    print("no param")
+
+				sendMessage(s,"PONG!")
+				break
 			if "*liquidslam" in message:
 			        sentence = cmdSillySentence()
 				if sentence:
@@ -62,8 +65,8 @@ while True:
 					print("Need more words!!!")
 					#sendMessage(s,"Not enough souls Kappa")
 				break
-			if "PING" in message:
-				PONG(s)
-				#s.send(line.replace("PING", "PONG"))
-				break
+			if ("*Kappa" in message):
+					sendMessage(s,kappaCount+" kappas")
+			
+			
 
