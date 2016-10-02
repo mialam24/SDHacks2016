6709f44fb469a87b1b7dbae23dda133a8c04be35
Ismail <ismail.alam2@gmail.com>
2016-10-02 01:48:08 -0700

    --

--- Settings.py
+++ Settings.py
@@ -1,5 +1,6 @@
 HOST = "irc.chat.twitch.tv"
 PORT = 6667
-PASS = "oauth:yox1a7c0c2h4g2o6rs338957i25g7w"
-IDENT = "test_bot1043"
-CHANNEL = "test1043"
+PASS = "oauth:nf9av6h6eim0sdu8cgbdmez05c4bli"
+IDENT = "lumilumi24"
+CHANNEL = "mendokusaii"
+RCRDINT = 10
