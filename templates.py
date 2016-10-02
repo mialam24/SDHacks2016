a01aea94b33a32bbac4237b656eaaa208a0686b4
Jeffrey Phung <jjphung@ucsd.edu>
2016-10-02 02:05:05 -0700

    more templates

--- templates.py
+++ templates.py
@@ -42,3 +42,45 @@ It is the East, and Juliet is the [CHAT]!")
 
 madlib_list.append("Any intelligent fool can make things [CHAT], more [CHAT], and more violent. \
 It takes a touch of genius -- and a lot of [CHAT] -- to move in the opposite [CHAT].")
+
+#10
+
+madlib_list.append("You are extremly [Word Not Submitted] and I [Word Not Submitted] you! I want kiss \
+your [Word Not Submitted] [Word Not Submitted] times. You make my [Word Not Submitted] burn with desire. \
+When I first saw you, I [Word Not Submitted] stared at you and fell in love. Will you [Word Not Submitted] out with me? \
+Don`t let your parents discourage you, [Word Not Submitted] are just jealous.")
+
+madlib_list.append("Be kind to your [Word Not Submitted]-footed [Word Not Submitted] \
+For a duck may be somebody`s [Word Not Submitted], \
+Be kind to your [Word Not Submitted] in [Word Not Submitted] \
+Where the weather is always [Word Not Submitted].")
+
+madlib_list.append("The [Word Not Submitted] Dragon is the [Word Not Submitted] Dragon of all. It has [Word Not Submitted] [Word Not Submitted], \
+and a [Word Not Submitted] shaped like a [Word Not Submitted].")
+
+madlib_list.append("Batman is [Word Not Submitted]. Teenager [Word Not Submitted] was traumatized by [Word Not Submitted] his \
+parent`s murder and vowed to [Word Not Submitted] their deaths by bringing the [Word Not Submitted] to justice.")
+
+madlib_list.append("In the film [Word Not Submitted] Wars: A New [Word Not Submitted], Princess [Word Not Submitted] sends out a call for help \
+right before she gets captured by [Word Not Submitted].")
+
+madlib_list.append("Once upon a time there was a [Word Not Submitted]. It had [Word Not Submitted] [Word Not Submitted]! One day it met [Word Not Submitted] \
+on the side of the [Word Not Submitted] they were [Word Not Submitted].")
+
+madlib_list.append("Tommy the gangsta [Word Not Submitted] was walking up the street to his [Word Not Submitted] in the [Word Not Submitted]. But just as \
+he came inside he saw that all his gangsta [Word Not Submitted] had been replaced with [Word Not Submitted] stuff.")
+
+madlib_list.append("In [Word Not Submitted]: [Word Not Submitted] of Time you play [Word Not Submitted], a young boy who has no [Word Not Submitted]. \
+In this adventure, he [Word Not Submitted] with his trusty [Word Not Submitted] and many other [Word Not Submitted] from his inventory.")
+
+madlib_list.append("It [Word Not Submitted] into the garden, \
+And under a [Word Not Submitted], \
+And then my [Word Not Submitted] [Word Not Submitted]ball, \
+Was nothing but mush. ")
+
+madlib_list.append("The tree was all covered, \
+All covered with [Word Not Submitted], \
+And on it grew [Word Not Submitted] balls, \
+And [Word Not Submitted]. ")
+
+#20
